a98165c2a41de15ec99b8614111b01826b316f11
Elijah <gemar19@mail.ru>
2018-03-18 15:55:42 +0300

    Find numbers in array that contain digit

---
+++ FilterDigitLib/FilterDigitApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilterDigitApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[1000000];
+            Random rand = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                array[i] = rand.Next(0, 200);
+            }
+            int digit = 7;
+        
+            Stopwatch stopwatchStr = Stopwatch.StartNew();
+            int[] newarray = FilterDigitLib.FilterDigit.FilterDigitUsingString(array,digit);
+            foreach (var item in newarray)
+            {
+                Console.Write(item + " ");
+            }
+            stopwatchStr.Stop();
+
+            Console.WriteLine();
+            Console.WriteLine(stopwatchStr.ElapsedMilliseconds + "ms");
+            Console.WriteLine();
+
+            Stopwatch stopwatchDiv = Stopwatch.StartNew();
+
+         
+            int[] newarr = FilterDigitLib.FilterDigit.FilterDigitByDiv(array, 7);
+            foreach (var item in newarr)
+            {
+                Console.Write(item + " ");
+            }
+            stopwatchDiv.Stop();
+            Console.WriteLine();
+            Console.WriteLine(stopwatchDiv.ElapsedMilliseconds + "ms");
+
+        }
+    }
+}
---
+++ FilterDigitLib/FilterDigitLib/FilterDigit.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilterDigitLib
+{
+    public class FilterDigit
+    {
+        #region FilterDigitUsingString
+
+        public static int[] FilterDigitUsingString(int[] numbers, int digit)
+        {
+
+            if (digit < 0 || digit > 9)
+            {
+                throw new ArgumentException($"Enter a {nameof(digit)} from 0 to 9!");
+            }
+
+            if (numbers.Length == 0)
+            {
+                return null;
+            }
+
+            if (numbers == null)
+            {
+                throw new ArgumentException(nameof(numbers));
+            }
+
+            int[] sortedArray = new int[0];
+            string stringDigit = digit.ToString();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                string number = numbers[i].ToString();
+                if (number.Contains(stringDigit))
+                {
+                    Array.Resize(ref sortedArray, sortedArray.Length + 1);
+                    sortedArray[sortedArray.Length - 1] = numbers[i];
+                }
+            }
+
+            return sortedArray;
+
+        }
+
+        #endregion
+
+        #region FilterDigitUsingDivisions
+        public static int[] FilterDigitByDiv(int[] array, int digit)
+        {
+            int[] newarray = new int[0];
+            foreach (var number in array)
+            {
+                Filter(number, digit);
+            }
+            foreach (var number in array)
+            {
+                var result = Filter(number, digit);
+                if (result != 0)
+                {
+                    Array.Resize(ref newarray, newarray.Length + 1);
+                    newarray[newarray.Length - 1] = result;
+                }
+            }
+
+            return newarray;
+        }
+
+        public static int Filter(int number, int digt)
+        {
+            int tmp = number;
+            while (tmp != 0)
+            {
+                if (tmp % 10 == digt)
+                { return number; }
+                tmp /= 10;
+            }
+
+            return 0;
+        }
+    }
+    #endregion
+}
+
+
